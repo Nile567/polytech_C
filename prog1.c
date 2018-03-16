8a674ec04da498117db6bec20f28cd7b9934a21d
Nile567 <32577913+Nile567@users.noreply.github.com>
2018-03-16 23:40:54 +0300

    Задача 1
    К сожалению на английском(

---
+++ prog1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>	
+int main(void)
+{
+	printf("1. In C language, all lowercase characters are significant\n");
+	printf("2. The word main indicates the beginning of the program execution\n");
+	printf("3. The opening and closing curly brackets contain the approval of the program\n");
+	printf("4. All statements of a program must end with a semicolon\n");
+	return 0;
+}
