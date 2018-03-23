2c89afd3f3499da6a411c582bcc43b35646f2141
Nile567 <32577913+Nile567@users.noreply.github.com>
2018-03-23 20:50:51 +0300

    Программа №4
    Программа из 4  главы

---
+++ numero4.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+
+int main(void) 
+{ 
+float F = 27.0; 
+float C = (F - 32) / 1.8; 
+printf("Ответ %.1f градусов по Фаренгейту в градусах Цельсия %.1f\n", F, C); 
+return 0; 
+}
